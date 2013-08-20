b8cf2f78274fa3bc64c59c649629002c2b464a81
Qinsi ZHU <qszhu2006@gmail.com>
2013-08-20 09:40:11 +0800

    keep screen on when using timer

--- src/qszhu/trakr/task/TaskTimerActivity.java
+++ src/qszhu/trakr/task/TaskTimerActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
@@ -39,6 +40,9 @@ public class TaskTimerActivity extends Activity implements OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.task_timer);
 
         Intent intent = getIntent();
